7032ac6e0424749d6249811d6a4d110ab6baa38e
Chris GannoN <cjgannon@gmail.com>
2013-07-02 17:23:07 +0100

    Initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post
+  include Mongoid::Document
+
+  field :title
+  field :published, type: DateTime, :default => Proc.new {  DateTime.now }
+  field :by
+  field :body
+
+  index({published:1})
+
+  default_scope order_by(:published => :desc)
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '1d85d69863046680629c37e16991c727972553b658e6428d81dabd5463fc2abde54151d76c10e5a202b524e8ed39c98492998b31666f9f3711927e3292d4bea5'
